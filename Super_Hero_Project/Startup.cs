e6388cc8061272c5124912621202d123b70a9298
Robe Gogan <robe.gogan@gmail.com>
2019-03-01 10:43:06 -0600

    Added the ASP.NET Framework to project and cloned local repo with virtual repo.

---
+++ Super_Hero_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Super_Hero_Project.Startup))]
+namespace Super_Hero_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
